da4ff5fa570de6db34f5eec36588a6bc6ca1ad8d
fog <fog.hua@gmail.com>
2022-11-16 11:22:53 +0800

    ! update files

--- gtr120_benv/r.sh
+++ gtr120_benv/r.sh
@@ -1,10 +1,11 @@
 GUESTUSER=docker
 USERHOME=/home/$USER
+GUESTHOME=/home/$GUESTUSER
 WORKSPACE=/workspace
 REPO=gtr120-buildenv:v1.0
 CN=gtr120-benv
 echo -n "Docker ID:"
 # --ipc="host" is a fix for share memory issue
 docker run --ipc="host" --name=$CN \
-    --rm -it --init -e DISPLAY -e LOCAL_USER_ID=`id -u $USER` \
+    --rm -it --init -e DISPLAY -e LOCAL_USER_ID=`id -u $USER` -w $GUESTHOME \
     -v $USERHOME:/home/$GUESTUSER -v $WORKSPACE:/workspace $REPO
